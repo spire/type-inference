749346c14b5fd94d80994942ca6419caa2137366
Nathan Collins <nathan.collins@gmail.com>
2013-11-20 16:05:12 -0800

    Update 'bug/open/SubstitutionBug.hs' to use monadic substitution: capture is gone!
    So, at least in this simple example, the capture is fixed / avoided.
    But, since six tests now fail, maybe I introduced other bugs?

--- bug/open/SubstitutionBug.hs
+++ bug/open/SubstitutionBug.hs
@@ -18,10 +18,12 @@ egs = runFreshM $ do
       n1 = N v1 B0
       eg1 = L (bind x0 (L (bind x1 (C (Pair n0 n1)))))
       eg2 = n1
+  eg1eg2 <- eg1 $$ eg2
   return $ [ eg1
            , eg2
-           , eg1 $$ eg2 -- Should be \ x2 . Pair x1 x2 for *fresh* x
-                        -- but it's  \ x1 . Pair x1 x1 instead :P
+           , eg1eg2
+           -- , eg1 $$ eg2 -- Should be \ x2 . Pair x1 x2 for *fresh* x
+           --              -- but it's  \ x1 . Pair x1 x1 instead :P
            ]
 
 main = do
